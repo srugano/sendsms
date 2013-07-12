6973353f84d6d8c9c369d61ec66714fb478b4e90
srugano <kuristockyrugano@gmail.com>
2013-07-12 14:17:47 +0200

    make due changes

--- sendsms/admin.py
+++ sendsms/admin.py
@@ -1,27 +1,11 @@
 #! /usr/bin/env python
 # encoding=utf-8
-
 from django.contrib import admin
-from sendsms.models import User, Questions, Responses, States
+from sendsms.models import Questions
 
-class UserAdmin(admin.ModelAdmin):
-    fields = ['first_name','telephone']
-    list_display = ('first_name','telephone')
 
 class QuestionsAdmin(admin.ModelAdmin):
-	fields = [ 'question']
-	list_display = ('numero','question','release_date')
+    #fields = ['question', 'reponse']
+    fields = ['question']
 
-class ResponsesAdmin(admin.ModelAdmin):
-	fields = ['response', 'question']
-	list_display = ('response','question','release_date' )
-
-class StatesAdmin(admin.ModelAdmin):
-	fields = ['user'  ] 
-	list_display = ('user','n_messages')
-	
-	
-admin.site.register(User, UserAdmin)
 admin.site.register(Questions, QuestionsAdmin)
-admin.site.register(Responses, ResponsesAdmin)
-admin.site.register(States, StatesAdmin)
