1f9707f0901ee10927ba6b240cc39e72ed9ccb47
srugano <kuristockyrugano@gmail.com>
2013-07-12 14:10:11 +0200

    make it behave like due

--- sendsms/app.py
+++ sendsms/app.py
@@ -1,31 +1,34 @@
 #!/usr/bin/env python
 # vim : ai ts=4 sts=4 et sw=4
-
-import datetime
-import time 
 from rapidsms.apps.base import AppBase
-from sendsms.models import *
-
+from sendsms.models import Questions, History
 
 class App(AppBase):
 	def handle(self, message):
-		entry = message.text.split(" ")
-		if entry[0] == 'joindre':
-			for oui in Questions.objects.all():
-				message.respond(oui.question)
-				Responses(
-				response = str(message.text),
-				release_date = datetime.datetime.now(),
-				question = oui ).save()
-				
-				def handle2(self, msg)
-					entryB = msg.text.split(" ")
-					if entryB[0] == 'oui':
-						continue
-					else:
-						break
-		return True
-		
-
+		tel=message.peer
+		msg=message.text.split(" ")
+		if msg[0]=='JO':
+			for q in Questions.objects.all():
+				h=History(question=q.question,tel_num=tel,status=0)
+				h.save()
+			message.respond("WELCOME TO RAPIDSMS")
+			return True
+		elif History.objects.filter(tel_num=tel):
+			for q in (History.objects.filter(tel_num=tel)):
+				if(q.status==1):
+					q.reponse=message.text
+					q.status=2
+					q.save()
+				if(q.status==0):
+					q.status=1
+					q.save()
+					quest=q.question
+					message.respond(" %s " %(quest))
+					return True
+			message.respond("CONGRATULATION! YOU ARE REGISTERED IN OUR SYSTEM !")
+			return True
+		else:
+			message.respond("SORY YOU ARE NOT REGISTERED! TO REGISTER YOURSELF SEND JO !")
+			return True
 		
 		
